9cd0c2ea93052e20539ecb5f8cd9ffcf47ea25c0
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2023-07-10 01:30:33 +0530

    last commit

--- JavaScript/loops/app.js
+++ JavaScript/loops/app.js
@@ -1,3 +1,18 @@
-for(let num=1;num<=100;num++){
+//for loop
+for(let num=1;num<=10;num++){
     console.log(num);
+}
+
+for(let n=1;n<=10;n++){
+    if(n==5){
+        break;
+    }
+    console.log(n);
+}
+console.log("nhi chal rha kya...");
+//while loop
+let i=1;
+while(i<=10){
+    console.log(i);
+    i++;
 }
