bc15eeb94124d52340fab346925f9bf827bda42a
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2023-07-09 23:32:10 +0530

    last commit

---
+++ JavaScript/operators/app.js
@@ -0,0 +1,10 @@
+const age=parseInt(prompt("Enter your age"))
+if(age<18){
+    alert("nikal ja yaha se")
+}
+else if(age>=18 && age<25){
+    alert("aaja lekin peena mt")
+}
+else{
+    alert("ab kyahi bolu ja pee le bina piye toh manega nhi ")
+}
