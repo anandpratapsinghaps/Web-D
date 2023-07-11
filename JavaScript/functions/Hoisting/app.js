19d4840958df97554dfd6fffdd2d219ec985cad5
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2023-07-12 01:24:09 +0530

    last commit

--- JavaScript/functions/Hoisting/app.js
+++ JavaScript/functions/Hoisting/app.js
@@ -10,3 +10,13 @@ function fun(){
     console.log("Inside Fun");
 }
 
+
+// hoisting in let and const is done but we can't access the variable before initialisation due to dead temporal zone
+console.log(b);
+let b=100;
+
+fun();
+function fun(){
+    console.log("Inside Fun");
+}
+
