9aa3c888ab5d64a15c1ea8a904edcc37f5602b2a
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2023-07-13 22:51:02 +0530

    last commit

--- JavaScript/functions/app.js
+++ JavaScript/functions/app.js
@@ -15,7 +15,20 @@ function sum(x,y){
 const result=sum(4,4);
 console.log(result);
 
+
+//default argument
 function Sum(x,y,z=0){
     return x+y+z;
 }
 console.log(Sum(1,2));
+
+
+// function expression
+const square=function(n){
+    return n * n;
+}
+console.log(square(5));
+
+// arrow function implicit return
+const cube=(n) => n*n*n;   //if there is only one argument we can remove () too
+console.log(cube(5));
