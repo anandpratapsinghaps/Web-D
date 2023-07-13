7048b3b0397818b84e245b1a57c1668670b57192
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2023-07-14 01:21:01 +0530

    last commit

---
+++ JavaScript/objects/app.js
@@ -0,0 +1,13 @@
+
+
+//object creation
+const person={
+    name:"Hannah",
+    age:20,
+    isAdult:true
+}
+
+//prototype or prototypal inheritance
+const p1=Object.create(person);
+const p2=Object.create(p1);
+
