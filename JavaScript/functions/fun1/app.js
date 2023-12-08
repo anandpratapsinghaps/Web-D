d332b3b06fbc76b77d01a615d5e8059b0ff24195
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2023-12-08 22:37:58 +0530

    last commit

--- JavaScript/functions/fun1/app.js
+++ JavaScript/functions/fun1/app.js
@@ -1,12 +1,36 @@
-var y=function (){
-    console.log("inside fun");
-    var a = 30;
-    return a;
-}
+// var y=function (){
+//     console.log("inside fun");
+//     var a = 30;
+//     return a;
+// }
 
-var p=y;
-var q=y();
-var r=q();
-console.log(p);
-console.log(q);
-console.log(r);
+// var p=y;
+// var q=y();
+// var r=q();
+// console.log(p);
+// console.log(q);
+// console.log(r);
+
+function outer(){
+    var money=50;
+    function inner(){
+        money++;
+        var a=10;
+        function innermost(){
+            money++;
+            a++;
+            console.log('money=',money,'a=',a);
+        }
+        return innermost;
+    }
+    return inner;
+}
+var fun=outer();
+var x=fun();
+var y=fun();
+x();
+x();
+x();
+y();
+y();
+x();
