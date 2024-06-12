67cade35573277c988d670a3fb9b44de3a6f566f
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-06-12 15:10:38 +0530

    last commit

--- JavaScript/loops/app.js
+++ JavaScript/loops/app.js
@@ -1,40 +1,11 @@
-//for loop
-for(let num=1;num<=10;num++){
-    console.log(num);
+const favMovie="avatar";
+let guess = prompt("Guess the movie");
+
+while((guess!=favMovie)&&(guess!="quit")){
+    guess=prompt("try again");
 }
-
-for(let n=1;n<=10;n++){
-    if(n==5){
-        break;
-    }
-    console.log(n);
-}
-console.log("nhi chal rha kya...");
-
-
-//while loop
-let i=1;
-while(i<=10){
-    console.log(i);
-    i++;
-}
-
-
-// for-of loop
-const animals=['Dog','Lion','Cat','Aur tum..'];
-
-for(let animal of animals){
-    console.log(animal);
-}
-
-
-// for-in loop
-const song={
-    name:"Nai Lagda",
-    singer:"Vishal Mishra",
-    mood:"sad",
-    feeling:"missing you"
-}
-for(let n in song) {
-    console.log(n + ":"+song[i]);
+if(guess==favMovie){
+    console.log("sahi hai bhai");
+} else{
+    console.log("rehne de tere se nhi hoga");
 }
