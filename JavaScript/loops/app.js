5cfff8447dee81d4d889776af405f8fb0a59763f
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-09-12 17:11:02 +0530

    last commit

--- JavaScript/loops/app.js
+++ JavaScript/loops/app.js
@@ -1,11 +1,53 @@
-const favMovie="avatar";
-let guess = prompt("Guess the movie");
+// const favMovie="avatar";
+// let guess = prompt("Guess the movie");
 
-while((guess!=favMovie)&&(guess!="quit")){
-    guess=prompt("try again");
+// while((guess!=favMovie)&&(guess!="quit")){
+//     guess=prompt("try again");
+// }
+// if(guess==favMovie){
+//     console.log("sahi hai bhai");
+// } else{
+//     console.log("rehne de tere se nhi hoga");
+// }
+
+
+//practice questions
+
+//delete all occurrences of num in arr
+let arr = [1, 2, 3, 4, 5, 6, 3, 8];
+let num = 3;
+for(let i=0; i<arr.length; i++){
+    if(arr[i] == num){
+        arr.splice(i, 1);
+    }
 }
-if(guess==favMovie){
-    console.log("sahi hai bhai");
-} else{
-    console.log("rehne de tere se nhi hoga");
+console.log(arr);
+
+//count the no of digit in a number
+let number = 123456;
+let count = 0;
+let copy = number;
+while(copy > 0){
+    count++;
+    copy = Math.floor(copy/10);
 }
+console.log(count);
+
+//sum of digits
+let nums = 123;
+let sum = 0;
+let n1 = nums;
+while(n1 > 0){
+    digit = n1%10;
+    sum += digit;
+    n1 = Math.floor(n1/10);
+}
+console.log(sum);
+
+//factorial
+let n = 7;
+let fact = 1;
+for(let i=1; i<=n; i++){
+    fact *= i;
+}
+console.log(`The factorial of ${n} is ${fact}`);
