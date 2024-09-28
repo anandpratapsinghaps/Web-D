e48dbc30e33f8868b682b542d0df4222dd3635ab
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-09-28 20:30:08 +0530

    last commit

--- JavaScript/functions/app.js
+++ JavaScript/functions/app.js
@@ -3,3 +3,17 @@ function rollDice(){   //function definition
     console.log(num);
 }
 rollDice(); //function calling
+
+function calAvg(a, b, c){
+    let avg = (a + b + c) / 3;
+    console.log(avg);
+}
+calAvg(2, 3, 4);
+
+//print table
+function table(n){
+    for(let i=n; i<=n*10; i+=n){
+        console.log(i);
+    }
+}
+table(2);
