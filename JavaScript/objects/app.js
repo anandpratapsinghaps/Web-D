d7d5e3f37d1d96debf3e64fe8bac001735027d3e
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-11-22 00:35:54 +0530

    last commit

--- JavaScript/objects/app.js
+++ JavaScript/objects/app.js
@@ -10,3 +10,20 @@ const person={
 const p1=Object.create(person);
 const p2=Object.create(p1);
 
+
+const student = {
+    name : "APS",
+    age : 23,
+    eng : 94,
+    math : 99,
+    phy : 98,
+
+    //method
+    getAvg(){
+        let avg = (this.eng + this.math + this.phy) / 3;
+        console.log(`${avg} is the avg marks of ${this.name}`);
+    }
+}
+
+
+
